5600d7215a57f71f2d92db7f867480ce387f9135
DimasHN <dimashn04@gmail.com>
2023-03-11 22:49:21 +0700

    fixed timeline

--- components/Requirements.jsx
+++ components/Requirements.jsx
@@ -7,7 +7,7 @@ const Requirements = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-orange-600 font-bold'>Requirements & Registration</p>
                 <p className='py-4 text-2xl font-bold'>See Requirements & Register Now</p>
-                <ol className='relative border-l border-orange-600 dark:border-gray-700'>
+                <ol className='relative dark:border-gray-700'>
                     <li className='mb-4 ml-4'>
                         <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                         <p class="font-semibold text-gray-900 dark:text-white">1. For Indonesian and foreign students</p>
@@ -49,7 +49,7 @@ const Requirements = () => {
                     </li>
                 </ol>
                 <p className='py-4 text-2xl font-bold'>Fee</p>
-                <ol className='relative border-l border-orange-600 dark:border-gray-700'>
+                <ol className='relative dark:border-gray-700'>
                     <li className='mb-4 ml-4'>
                         <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                         <p class="font-semibold text-gray-900 dark:text-white">Foreign student: US$ 300</p>
@@ -66,7 +66,7 @@ const Requirements = () => {
                     </li>
                 </ol>
                 <p className='text-green-600 font-bold py-4'>Early Bird Registration (until 28 April 2023):</p>
-                <ol className='relative border-l border-orange-600 dark:border-gray-700'>
+                <ol className='relative dark:border-gray-700'>
                     <li className='mb-4 ml-4'>
                         <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                         <p class="font-semibold text-gray-900 dark:text-white">Foreign student: US$ 250 </p>
@@ -77,8 +77,8 @@ const Requirements = () => {
                         {/* <a href="https://forms.gle/L8mN4y4enAVmgPMfA" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Register now <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a> */}
                     </li>
                 </ol>
-                <p className='py-2 font-bold text-blue-500'>Latest date of Registration form	: Monday, 12 June 2023</p>
-                <p className='pb-2 font-bold text-blue-500'>Latest date of the Payment	: Thursday, 15 June 2023</p>
+                <p className='py-2 font-bold text-blue-500'>Final date of Registration	: Monday, 12 June 2023</p>
+                <p className='pb-2 font-bold text-blue-500'>Final date of the Payment	: Thursday, 15 June 2023</p>
                 <p className='pb-2'>Transfer to <span className='font-bold hover:border-b border-orange-400'><a href='https://docs.google.com/document/d/1Y15zm-RiIBhTT320NYQyW4gf-PPV3V4P/edit?usp=share_link&ouid=112279601469461014351&rtpof=true&sd=true'>(see link in official statement letter)</a></span></p>
                 <p className='pb-2 font-bold text-red-500'>* transfer fee, airfare, visa, meals accommodation are excluded</p>
             </div>
--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Timeline = () => {
@@ -10,19 +11,43 @@ const Timeline = () => {
                 <ol className='relative border-l border-orange-600 dark:border-gray-700'>
                     <li className='mb-4 ml-4'>
                         <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">26 June 2023</time>
-                        <p class="font-semibold text-gray-900 dark:text-white">Registration starts</p>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">11 March 2023</time>
+                        <p class="font-semibold text-gray-900 dark:text-white">Registration and payment starts<br/>(Including early bird registration with benefits)</p>
                         <a href="https://forms.gle/L8mN4y4enAVmgPMfA" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Register now <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>
                     </li>
                     <li className='mb-4 ml-4'>
                         <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">28 April 2023</time>
+                        <p class="font-bold text-red-600 dark:text-white">Early bird registration closes</p>
+                        {/* <a href="https://forms.gle/L8mN4y4enAVmgPMfA" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Register now <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a> */}
+                    </li>
+                    <li className='mb-4 ml-4'>
+                        <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">12 June 2023</time>
+                        <p class="font-bold text-red-600 dark:text-white">Registration closes</p>
+                        {/* <a href="https://forms.gle/L8mN4y4enAVmgPMfA" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Register now <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a> */}
+                    </li>
+                    <li className='mb-4 ml-4'>
+                        <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">15 June 2023</time>
+                        <p class="font-bold text-red-600 dark:text-white">Payment closes</p>
+                        {/* <a href="https://forms.gle/L8mN4y4enAVmgPMfA" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Register now <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a> */}
+                    </li>
+                    <li className='mb-4 ml-4'>
+                        <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">26 June 2023</time>
+                        <p class="font-semibold text-gray-900 dark:text-white">Event starts</p>
+                        <Link href="/#activities" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">See activities <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></Link>
+                    </li>
+                    <li className='mb-4 ml-4'>
+                        <div class="absolute w-3 h-3 bg-gray-600 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                         <time class="mb-1 text-sm font-normal leading-none text-gray-700 dark:text-gray-800">7 July 2023</time>
-                        <p class="font-semibold text-gray-900 dark:text-white">Registration ends</p>
+                        <p class="font-bold text-red-600 dark:text-white">Event ends</p>
                     </li>
                 </ol>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                <img className='rounded-xl' src="https://i.imgur.com/7dF9SkF.png" alt="" />
+                <img className='rounded-xl' src="https://media.istockphoto.com/id/612235546/photo/calendar.jpg?s=170667a&w=0&k=20&c=pZGUy-yy36MhqzTgOyQnQ6oHpuMqFq1k7uznlbCnH2o=" alt="" />
             </div>
         </div>
     </div>
