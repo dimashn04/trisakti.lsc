6b9e2c03ff58eee66e2bc3d2cbf97ce71bf62fa8
DimasHN_04 <dimashn04@gmail.com>
2024-02-07 13:03:20 +0700

    Fixed error

--- components/About.jsx
+++ components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[#8C323E] font-bold'>About</p>
                 <p className='py-4 text-2xl font-bold text-[#574821]'>Join Us</p>
-                <p className='py-2 text-gray-700'>It's our third Trisakti Law Summer Course....... so exciting. Joining Trisakti law summer course is fun to study in holiday time. Get experience in a new country, culture, learn the legal system and basics of a new language, and of course gain friends from all over the world.</p>
+                <p className='py-2 text-gray-700'>It&apos;s our third Trisakti Law Summer Course....... so exciting. Joining Trisakti law summer course is fun to study in holiday time. Get experience in a new country, culture, learn the legal system and basics of a new language, and of course gain friends from all over the world.</p>
                 <p className='py-2 text-gray-700'>Earn benefits such as a souvenir bag which includes a notepad and a pen and also a certificate.</p>
 
                 <p className='py-4 cursor-pointer font-bold text-[#574821]'><span className='hover:border-b border-[#8C323E]'><Link href='/#timeline'>Click here to check out the timeline</Link></span></p>
